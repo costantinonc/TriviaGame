abfe5b5b7fe4aea1c29988ebeeb71e747d04fbb0
costantinonc <costantinonc@gmail.com>
2017-09-26 20:02:49 -0400

    upload6545

---
+++ Assets/javascript/app.js
@@ -0,0 +1,174 @@
+var panel = $("#quiz-area");
+
+// Question set
+var questions = [{
+  question: "What year was I born in?",
+  answers: ["1991", "1988", "1985", "1992"],
+  correctAnswer: "Toy Story"
+}, {
+  question: "How tall am I?",
+  answers: ["5'11", "5'10", "6'2", "6'1"],
+  correctAnswer: "6'1"
+}, {
+  question: "What is my favorite sports team",
+  answers: ["NY RANGERS", "NY GIANTS", "NY KNICKS", "NY YANKEES"],
+  correctAnswer: "NY GIANTS"
+}, {
+  question: "What is my favorite food",
+  answers: ["Chicken Parm", "Pizza", "Tacos", "Steak"],
+  correctAnswer: "Chicken Parm"
+}, {
+  question: "What is my favorite movie",
+  answers: ["True Romance", "Remember The Titans", "Pulp Fiction", "Gone Baby Gone"],
+  correctAnswer: "True Romance"
+}, {
+  question: "Where did I go to College",
+  answers: ["UNCC", "Coastal Carolina", "Clemson", "USC"],
+  correctAnswer: "Coastal Carolina"
+}, {
+  question: "What is my favorite tv show",
+  answers: ["Hard Knocks", "Ozark", "American Crime", "The Sopranos"],
+  correctAnswer: "The Sopranos"
+}, {
+  question: "What was the name of my highschool?",
+  answers: ["Albany Academy", "Christian Brothers Academy", "Shaker High", "Lasalle Institute"],
+  correctAnswer: "Christian Brothers Academy"
+}];
+
+// Variable that will hold the setInterval
+var timer;
+
+var game = {
+
+  correct: 0,
+  incorrect: 0,
+  counter: 120,
+
+  countdown: function() {
+    game.counter--;
+    $("#counter-number").html(game.counter);
+    if (game.counter === 0) {
+      console.log("TIME UP");
+      game.done();
+    }
+  },
+
+  start: function() {
+    timer = setInterval(game.countdown, 1000);
+
+    $("#sub-wrapper").prepend("<h2>Time Remaining: <span id='counter-number'>120</span> Seconds</h2>");
+
+    $("#start").remove();
+
+    for (var i = 0; i < questions.length; i++) {
+      panel.append("<h2>" + questions[i].question + "</h2>");
+      for (var j = 0; j < questions[i].answers.length; j++) {
+        panel.append("<input type='radio' name='question-" + i +
+        "' value='" + questions[i].answers[j] + "''>" + questions[i].answers[j]);
+      }
+    }
+
+    panel.append("<button id='done'>Done</button>");
+  },
+
+  done: function() {
+
+    $.each($("input[name='question-0']:checked"), function() {
+      if ($(this).val() === questions[0].correctAnswer) {
+        game.correct++;
+      }
+      else {
+        game.incorrect++;
+      }
+    });
+
+    $.each($("input[name='question-1']:checked"), function() {
+      if ($(this).val() === questions[1].correctAnswer) {
+        game.correct++;
+      }
+      else {
+        game.incorrect++;
+      }
+    });
+
+    $.each($("input[name='question-2']:checked"), function() {
+      if ($(this).val() === questions[2].correctAnswer) {
+        game.correct++;
+      }
+      else {
+        game.incorrect++;
+      }
+    });
+
+    $.each($("input[name='question-3']:checked"), function() {
+      if ($(this).val() === questions[3].correctAnswer) {
+        game.correct++;
+      }
+      else {
+        game.incorrect++;
+      }
+    });
+
+    $.each($("input[name='question-4']:checked"), function() {
+      if ($(this).val() === questions[4].correctAnswer) {
+        game.correct++;
+      }
+      else {
+        game.incorrect++;
+      }
+    });
+
+    $.each($("input[name='question-5']:checked"), function() {
+      if ($(this).val() === questions[5].correctAnswer) {
+        game.correct++;
+      }
+      else {
+        game.incorrect++;
+      }
+    });
+
+    $.each($("input[name='question-6']:checked"), function() {
+      if ($(this).val() === questions[6].correctAnswer) {
+        game.correct++;
+      }
+      else {
+        game.incorrect++;
+      }
+    });
+
+    $.each($("input[name='question-7']:checked"), function() {
+      if ($(this).val() === questions[7].correctAnswer) {
+        game.correct++;
+      }
+      else {
+        game.incorrect++;
+      }
+    });
+
+    this.result();
+
+  },
+
+  result: function() {
+
+    clearInterval(timer);
+
+    $("#sub-wrapper h2").remove();
+
+    panel.html("<h2>All Done!</h2>");
+    panel.append("<h3>Correct Answers: " + this.correct + "</h3>");
+    panel.append("<h3>Incorrect Answers: " + this.incorrect + "</h3>");
+    panel.append("<h3>Unanswered: " + (questions.length - (this.incorrect + this.correct)) + "</h3>");
+  }
+};
+
+// CLICK EVENTS
+
+$(document).on("click", "#start", function() {
+  game.start();
+});
+
+
+$(document).on("click", "#done", function() {
+  game.done();
+});
